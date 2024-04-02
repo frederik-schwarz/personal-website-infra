09db9d4077853393aab8205a74360ddb340f5aa7
frederik <frederik@jens-schwarz.com>
2024-04-02 18:53:19 +1300

    Create subent data blocks

--- data.tf
+++ data.tf
@@ -1,3 +1,18 @@
 data "aws_vpc" "main" {
   id = var.vpc_id
 }
+
+data "aws_subnet" "main_public_a" {
+  filter {
+    name   = "tag:Name"
+    values = ["Main-public-b"]
+  }
+}
+
+data "aws_subnet" "main_public_a" {
+  filter {
+    name   = "tag:Name"
+    values = ["Main-public-b"]
+  }
+}
+
