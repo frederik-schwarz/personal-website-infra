cc0701cb2a8136f43f8fc4f463b5d6792f5c0ca7
frederik <frederik@jens-schwarz.com>
2024-04-07 21:01:08 +1200

    Update date security group

--- ecs.tf
+++ ecs.tf
@@ -3,9 +3,10 @@ resource "aws_ecs_service" "main" {
   cluster         = data.aws_ecs_cluster.fargate.id
   task_definition = aws_ecs_task_definition.personal_website.arn
   desired_count   = 1
+  launch_type     = "FARGATE"
   network_configuration {
     subnets = [data.aws_subnet.main_private_a.id, data.aws_subnet.main_private_b.id]
-    security_groups = []
+    security_groups = [aws_security_group.ecs.id]
   }
   load_balancer {
     target_group_arn = aws_lb_target_group.main.arn
@@ -20,7 +21,9 @@ resource "aws_ecs_task_definition" "personal_website" {
   network_mode             = "awsvpc"
   cpu                      = 256 
   memory                   = 512 
-  container_definitions = jsonencode([
+  execution_role_arn       = "arn:aws:iam::897577706574:role/ecsTaskExecutionRole"
+  task_role_arn            = aws_iam_role.fargate.arn
+  container_definitions    = jsonencode([
     {
       name      = "personal-website"
       image     = "personal-website"
@@ -36,19 +39,19 @@ resource "aws_ecs_task_definition" "personal_website" {
 }
 
 resource "aws_security_group" "ecs" {
-  name        = "alb_main"
+  name        = "ecs"
   description = "Security Group attached to alb"
   vpc_id      = data.aws_vpc.main.id
 }
 
-resource "aws_security_group_rule" "egress_anywhere" {
-  security_group_id = aws_security_group.alb_main.id
+resource "aws_security_group_rule" "ecs_egress_anywhere" {
+  security_group_id = aws_security_group.ecs.id
   type              = "egress"
   protocol          = "-1"
   from_port         = 0
   to_port           = 0
   cidr_blocks       = ["0.0.0.0/0"]
-  ipv6_cidr_blocks  = ["::"]
+  ipv6_cidr_blocks  = ["::/0"]
 }
 
 resource "aws_security_group_rule" "alb_ingress" {
